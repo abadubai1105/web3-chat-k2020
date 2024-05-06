02836ad0fab13a3b70bc086b10f98dda00207de2
MeeSay <huycqter@gmail.com>
2024-05-06 22:38:18 +0700

    Test: chat

--- updated-code/hardhat.config.js
+++ updated-code/hardhat.config.js
@@ -1,11 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { PRIVATE_KEY } = process.env;
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+//   networks: {
+//       url: `http://localhost:8545`,
+//       accounts: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+//   },
+// };
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-    hardhat: {
-      chainId: 31337,
-    },
-  },
+    networks: {
+    //   hardhat: {
+    //    // Localhost with default Hardhat network settings
+    //  },
+     localhost: {
+        url: "http://localhost:9545", // URL of your local chain
+        accounts: [process.env.PRIVATE_KEY], // Private key for local deployment
+     },
+   },
 };
