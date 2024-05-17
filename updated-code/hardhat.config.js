5a7cfb36c909f3e85c221c31ead3c9f9110ad7d9
chnghia1105 <89822010+abadubai1105@users.noreply.github.com>
2024-05-17 19:03:24 +0700

    Revert "Revert "Revert "add searching user"""

--- updated-code/hardhat.config.js
+++ updated-code/hardhat.config.js
@@ -24,5 +24,9 @@ module.exports = {
         url: "http://localhost:9545", // URL of your local chain
         accounts: [process.env.PRIVATE_KEY], // Private key for local deployment
      },
+     localhost2: {
+      url: "http://localhost:8545", // URL of your local chain
+      accounts: [process.env.PRIVATE_KEY], // Private key for local deployment
+   },
    },
 };
