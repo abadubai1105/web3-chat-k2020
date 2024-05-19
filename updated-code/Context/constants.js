f0efe44ccf563b786a96d44c5380a6f9f4df947c
penyuBow <dangafk9999@gmail.com>
2024-05-19 17:21:52 +0700

    D

--- updated-code/Context/constants.js
+++ updated-code/Context/constants.js
@@ -1,8 +1,8 @@
 //POLYGON DEPLOY ADDRESS= 0xA68BDd8c7b77f53B0653AAc61C719f6397C8FA61
 
-import chatApp from "./ChatApp.json";
+import chatApp from "../artifacts/contracts/ChatApp.sol/ChatApp.json";
 
 // export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const ChatAppAddress = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
+export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 export const ChatAppABI = chatApp.abi;
