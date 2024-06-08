537f1a16b9d8af1cd590d0396433083fc80f96bb
penyuBow <dangafk9999@gmail.com>
2024-06-08 17:06:30 +0700

    delete contract khong can

--- updated-code/Context/constants.js
+++ updated-code/Context/constants.js
@@ -5,24 +5,6 @@ import chatApp from "../artifacts/contracts/ChatApp.sol/ChatApp.json";
 
 
 // export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-var fs = require("fs"); // Import mô-đun fs với promises
-
-let fileContent;
-
-
-
-//Hàm để đọc nội dung của tệp và gán giá trị cho ChatAppAddress
-function readContractAddress() {
-  try {
-    const content = fs.readFile("../updated-code/contract-address.txt",(error,data) => {});
-    return content.toString();
-  } catch (error) {
-    console.error('Error reading file:', error);
-    return null;
-  }
-}
-fileContent = readContractAddress();
-console.log(fileContent);
 // Gọi hàm readContractAddress và export ChatAppAddress
 export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const ChatAppABI = chatApp.abi;
