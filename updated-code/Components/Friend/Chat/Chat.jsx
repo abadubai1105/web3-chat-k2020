588ba8369fad338f3cdaf9f7999595ca67bd9822
penyuBow <dangafk9999@gmail.com>
2024-07-01 03:16:59 +0700

    Update fix error loading message

--- updated-code/Components/Friend/Chat/Chat.jsx
+++ updated-code/Components/Friend/Chat/Chat.jsx
@@ -34,12 +34,16 @@ const Chat = ({
     setChatData(router.query);
   }, [router.isReady, router.query]);
 
-  useEffect(() => {
-    if (chatData.address) {
-      readMessage(chatData.address);
-      readUser(chatData.address);
-    }
-  }, [chatData.address, readMessage, readUser]);
+  // const check = async () => {
+  //   if (chatData.address) {
+  //     //console.log(chatData.address);
+  //     await readMessage(chatData.address);
+  //     await readUser(chatData.address);
+  //   }
+  // }
+  // useEffect(() => {
+  //   check();
+  // }, [chatData.address]);//[chatData.address, readMessage, readUser]);
 
   return (
     <div className={Style.Chat}>
